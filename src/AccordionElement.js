27cdb45aca657044d8c17efa7efb58effceb2dda
ploiu <ploiu415@gmail.com>
2021-07-30 08:06:22 -0400

    class private properties (all major browsers support it now)

--- src/AccordionElement.js
+++ src/AccordionElement.js
@@ -15,20 +15,28 @@ if (!customElements.get('accordion-element')) {
 	 * - `data-open`: sets the accordion to be open after it's added to the dom.
 	 */
 	class AccordionElement extends HTMLElement {
+		#title
+		#id
+		#isOpen
+		#group
+		#titleElement
+		#bodyElement
+		#parentAccordion
+		#initialParentAccordionHeight
 		constructor({title, id, isOpen, group} = {}) {
 			super();
 			// TODO make these variables private once all major browsers support private variables
-			this._title = title ?? this.dataset.title ?? '';
-			this._id = id ?? this.dataset.id ?? idCounter++;
-			this._isOpen = isOpen ?? this.dataset.isOpen ?? false;
-			this._group = group ?? this.dataset.group ?? '';
-			this._titleElement = this._createTitleElement();
-			this._bodyElement = this._createBodyElement();
+			this.#title = title ?? this.dataset.title ?? '';
+			this.#id = id ?? this.dataset.id ?? idCounter++;
+			this.#isOpen = isOpen ?? this.dataset.isOpen ?? false;
+			this.#group = group ?? this.dataset.group ?? '';
+			this.#titleElement = this._createTitleElement();
+			this.#bodyElement = this._createBodyElement();
 
 			// add the accordion to the list
 			window.accordions.push(this);
 			
-			this._parentAccordion = null;
+			this.#parentAccordion = null;
 		}
 
 		// ==============================================================================
@@ -37,7 +45,7 @@ if (!customElements.get('accordion-element')) {
 		_createTitleElement() {
 			const element = document.createElement('div');
 			element.classList.add('ploiu-accordion-title');
-			element.innerText = this._title;
+			element.innerText = this.#title;
 			element.addEventListener('click', () => this.toggle());
 			return element;
 		}
@@ -60,29 +68,29 @@ if (!customElements.get('accordion-element')) {
 		 * @returns {number}
 		 */
 		get id() {
-			return this._id;
+			return this.#id;
 		}
 
 		get group() {
-			return this._group;
+			return this.#group;
 		}
 
 		set group(value) {
-			this._group = value;
+			this.#group = value;
 			this.dataset.group = value;
 		}
 
 		get title() {
-			return this._title;
+			return this.#title;
 		}
 
 		set title(value) {
-			this._title = value;
-			this._titleElement.innerText = value;
+			this.#title = value;
+			this.#titleElement.innerText = value;
 		}
 
 		get isOpen() {
-			return this._isOpen;
+			return this.#isOpen;
 		}
 
 		/**
@@ -92,7 +100,7 @@ if (!customElements.get('accordion-element')) {
 		 * @private
 		 */
 		get bodyElement() {
-			return this._bodyElement;
+			return this.#bodyElement;
 		}
 
 		/**
@@ -101,9 +109,9 @@ if (!customElements.get('accordion-element')) {
 		 */
 		set body(value) {
 			// first remove all the contents from our accordion body
-			Array.from(this._bodyElement.children).forEach(el => el?.remove());
+			Array.from(this.#bodyElement.children).forEach(el => el?.remove());
 			// now add the contents of the value to the body element
-			this._bodyElement.appendChild(value);
+			this.#bodyElement.appendChild(value);
 		}
 
 		/**
@@ -111,15 +119,15 @@ if (!customElements.get('accordion-element')) {
 		 */
 		expand() {
 			// hide all other open accordions in the same group
-			AccordionElement.findAccordionsByGroup(this._group).filter(accordion => accordion !== this && accordion !== this._parentAccordion).forEach(accordion => accordion.collapse());
-			this._isOpen = true;
+			AccordionElement.findAccordionsByGroup(this.#group).filter(accordion => accordion !== this && accordion !== this.#parentAccordion).forEach(accordion => accordion.collapse());
+			this.#isOpen = true;
 			// set the height of the body to be the scroll height of the body and all other accordion bodies underneath
-			this._bodyElement.style.height = `${this._bodyElement.scrollHeight}px`;
+			this.#bodyElement.style.height = `${this.#bodyElement.scrollHeight}px`;
 			// if the parent element is an accordion, increase the height of it to accommodate
-			if (this._parentAccordion?.isOpen) {
+			if (this.#parentAccordion?.isOpen) {
 				// add the height of our accordion minus the height of our title element. 
-				this._parentAccordion.bodyElement.style.height =
-					Number.parseFloat(this._parentAccordion.bodyElement.style.height.replace(/px/, '')) // we manually set the pixel height of the element, so this is ok to do
+				this.#parentAccordion.bodyElement.style.height =
+					Number.parseFloat(this.#parentAccordion.bodyElement.style.height.replace(/px/, '')) // we manually set the pixel height of the element, so this is ok to do
 					+ this.bodyElement.scrollHeight + 'px';
 			}
 			this.classList.add('open');
@@ -129,13 +137,13 @@ if (!customElements.get('accordion-element')) {
 		 * Closes the accordion and hides the contents inside of it
 		 */
 		collapse() {
-			if (this._isOpen) {
-				this._isOpen = false;
+			if (this.#isOpen) {
+				this.#isOpen = false;
 				// if our parent is an accordion element, reduce the size back to the initial height
-				if (this._parentAccordion) {
-					this._parentAccordion.bodyElement.style.height = this._parentAccordion.bodyElement.clientHeight - this.bodyElement.scrollHeight + 'px';
+				if (this.#parentAccordion) {
+					this.#parentAccordion.bodyElement.style.height = this.#parentAccordion.bodyElement.clientHeight - this.bodyElement.scrollHeight + 'px';
 				}
-				this._bodyElement.style.height = '0';
+				this.#bodyElement.style.height = '0';
 				this.classList.remove('open');
 				// close all child accordions
 				Array.from(this.querySelectorAll('accordion-element')).forEach(accordion => accordion.collapse());
@@ -146,7 +154,7 @@ if (!customElements.get('accordion-element')) {
 		 * Toggles the open/closed state of the accordion
 		 */
 		toggle() {
-			if (this._isOpen) {
+			if (this.#isOpen) {
 				this.collapse();
 			} else {
 				this.expand();
@@ -180,17 +188,17 @@ if (!customElements.get('accordion-element')) {
 			// make sure we're actually connected before we do anything
 			if (this.isConnected) {
 				// append children here and set up other dom-based attributes
-				this.appendChild(this._titleElement);
-				this.appendChild(this._bodyElement);
+				this.appendChild(this.#titleElement);
+				this.appendChild(this.#bodyElement);
 				// if the data-open attribute is set, expand this accordion
-				if (this.dataset.open !== undefined || this._isOpen) {
+				if (this.dataset.open !== undefined || this.#isOpen) {
 					this.expand();
 				}
-				this._parentAccordion = this._getParentAccordion();
-				if (this._parentAccordion !== null) {
-					this._initialParentAccordionHeight = this._parentAccordion.scrollHeight;
+				this.#parentAccordion = this._getParentAccordion();
+				if (this.#parentAccordion !== null) {
+					this.#initialParentAccordionHeight = this.#parentAccordion.scrollHeight;
 				} else {
-					this._initialParentAccordionHeight = 0; // reset this
+					this.#initialParentAccordionHeight = 0; // reset this
 				}
 			}
 		}
